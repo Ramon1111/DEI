fc4471655d59c5bd25466f7f032acd29ea361c66
root <jramoncito7-99@gmail.com>
2019-06-10 22:33:55 +0100

    [FIX] Modified open and close door 1

--- Templates/abrirCerrar.php
+++ Templates/abrirCerrar.php
@@ -1,14 +1,14 @@
 <?php
   $value=$_POST['estado'];
   if ($value==0){
-    $command = escapeshellcmd('python ../sensors/exampleOpen.py');
+    $command = escapeshellcmd('python ../sensors/openDoor1.py');
     //shell_exec($command);
     $newString=Shell_exec($command);
     sleep(1);
     echo $newString;
   }
   elseif($value==1){
-    $command = escapeshellcmd('python ../sensors/exampleClose.py');
+    $command = escapeshellcmd('python ../sensors/closeDoor1.py');
     $newString=shell_exec($command);
     echo $newString;
   }
