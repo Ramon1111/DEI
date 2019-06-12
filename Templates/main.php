ff8649fbfe702c66c51bee2a922359603c62c7c5
Ramon1111 <jramoncito7-99@hotmail.com>
2019-06-11 19:09:16 -0500

    [FIX] Change in main page

--- Templates/main.php
+++ Templates/main.php
@@ -38,80 +38,103 @@
             <div class="textBoton text-center">Botón para abrir y cerrar la puerta</div>
           </div>
           <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6 text-center my-2">
-            <button type="button" id="openClose" class="btn btn-primary btn-lg btn-block">Abrir puerta 1</button>
+            <button type="button" id="openClose1" class="btn btn-primary btn-lg btn-block">Abrir puerta 1</button>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6 my-2">
+            <div class="textBoton text-center">Botón para abrir y cerrar la puerta</div>
+          </div>
+          <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6 text-center my-2">
+            <button type="button" id="openClose2" class="btn btn-primary btn-lg btn-block">Abrir puerta 2</button>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6 my-2">
+            <div class="textBoton text-center">Consultar temperatura</div>
+          </div>
+          <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6 text-center my-2">
+            <button type="button" id="askTemperature" class="btn btn-primary btn-lg btn-block">Checar temperatura</button>
           </div>
         </div>
       </div>
       <script src="https://cdn.pubnub.com/sdk/javascript/pubnub.4.21.7.min.js">
       </script>
       <script>
-        $('#openClose').click(function(){
+        var channel = 'pubnub_onboarding_channel';
+
+        var pubnub= new PubNub({
+            subscribeKey:'sub-c-1c31c1b8-8bcb-11e9-9769-e24cdeae5ee1',
+            publishKey: 'pub-c-4872297c-41b7-4f68-ad26-ed2061493795',
+            ssl: true
+        });
+        pubnub.subscribe({
+          channels: ['pubnub_onboarding_channel']
+        });
+
+        $('#openClose1').click(function(){
           console.log($(this).html());
-          var channel = 'pubnub_onboarding_channel';
 
-          var pubnub= new PubNub({
-              subscribeKey:'sub-c-1c31c1b8-8bcb-11e9-9769-e24cdeae5ee1',
-              publishKey: 'pub-c-4872297c-41b7-4f68-ad26-ed2061493795',
-          });
-
-              alert("Test: Send message to pi");//Testing only
-              /*pubnub.publish({
-                  channel: channel,
-                  message: 'on'
-              });*/
+          alert("Test: Send message to pi");
           if($(this).html()=="Abrir puerta 1"){
             pubnub.publish({
                 channel: channel,
-                message: 'abrir'
+                message: 'abrir1'
             });
-            $('#openClose').html('Cerrar puerta 1');
-            /*$(this).html('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Loading...');
-            //estado 0 significa "CERRADO", o sea, hay que abrir la puerta
-            $.ajax({
-              url:"abrirCerrar.php",
-              //url: "../sensors/openDoor1.py"
-              data:{
-               	estado: 0
-              },
-              type: "POST",
-              dataType: "Text",
-              success: function(data){
-                console.log(data);
-                $('#openClose').html('Cerrar puerta 1');
-              }
-            });*/
+            $('#openClose1').html('Cerrar puerta 1');
           }
 
           else if($(this).html()=="Cerrar puerta 1"){
             pubnub.publish({
                 channel: channel,
-                message: 'cerrar'
+                message: 'cerrar1'
             });
-            $('#openClose').html('Abrir puerta 1');
-            /*$(this).html('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Loading...');
-            //estado 0 significa "CERRADO", o sea, hay que abrir la puerta
-            $.ajax({
-              url:"abrirCerrar.php",
-              data:{
-                estado: 1
-              },
-              type: "POST",
-              dataType: "Text",
-              success: function(data){
-                console.log(data);
-                $('#openClose').html('Abrir puerta 1');
-              }
-            });*/
+            $('#openClose1').html('Abrir puerta 1');
           }
+        });
+
+        $('#openClose2').click(function(){
+          console.log($(this).html());
+
+          alert("Test: Send message to pi");
+          if($(this).html()=="Abrir puerta 2"){
+            pubnub.publish({
+                channel: channel,
+                message: 'abrir2'
+            });
+            $('#openClose2').html('Cerrar puerta 2');
+          }
+
+          else if($(this).html()=="Cerrar puerta 2"){
+            pubnub.publish({
+                channel: channel,
+                message: 'cerrar2'
+            });
+            $('#openClose2').html('Abrir puerta 2');
+          }
+        });
+
+
+        $('#askTemperature').click(function(){
+            pubnub.publish({
+                channel: channel,
+                message: 'temperatura'
+            });
+        });
+
+        pubnub.addListener({
+            message: function(msg) {
+                if(msg.message.tipo=="1")
+                  alert("Aiua, se quema tu casa"+msg.message.mensaje);
+                else if (msg.message.tipo=="2")
+                  alert(msg.message.mensaje);
+            },
+            presence: function(presenceEvent) {
+                // handle presence
+            }
         });
       </script>
     </body>
     </html>
-  <?php
-
-  //$command = escapeshellcmd('python testSendN.py');
-  //$newString = shell_exec($command);
-  #$newString=exec(testSendN.py);
-
-  //echo $newString;
-?>
